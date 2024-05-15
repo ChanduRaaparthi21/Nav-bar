31de05df630c655a462bebee068ea27f8ae6561a
Chandu Raparthi <chandu.raparthi@gigleaz.com>
2024-05-15 13:16:08 +0530

    1.2

--- js.js
+++ js.js
@@ -1,6 +1,14 @@
 let toggle = document.querySelector(".toggle");
 let menu = document.querySelector(".menu");
 
+
+
+toggle.addEventListener("click", function() {
+  menu.classList.toggle("active");
+});
+
+
+
 window.addEventListener("resize", checkWidth);
 checkWidth();
 
@@ -11,7 +19,3 @@ function checkWidth() {
     toggle.style.display = "block";
   }
 }
-
-toggle.addEventListener("click", function() {
-  menu.classList.toggle("active");
-});
